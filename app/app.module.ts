a921649e0685a66807f3930f7b64e5c67c8dbd57
Bharathkumar V <bharathkumar.vaitheeswaran@adp.com>
2017-04-16 21:55:36 +0530

    Added location validator

--- app/app.module.ts
+++ app/app.module.ts
@@ -17,7 +17,8 @@ import {
     SessionListComponent,
     DurationPipe,
     UpvoteComponent,
-    VoterService
+    VoterService,
+    LocationValidator
 } from './events/index'
 
 import {EventsAppComponent} from './events-app.component'
@@ -59,7 +60,8 @@ declare let jQuery : Object;
         DurationPipe,
         SimpleModalComponent,
         ModalTriggerDirective,
-        UpvoteComponent
+        UpvoteComponent,
+        LocationValidator
     ],
     bootstrap:[EventsAppComponent],
     providers:[
